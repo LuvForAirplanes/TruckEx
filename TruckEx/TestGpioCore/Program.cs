63f18b1d33129ce8a7b8458f385d85cca6e41226
Micah Stauffer <micah@abcrun.com>
2018-06-03 15:59:56 -0400

    Add Program to test GPIO control using Bitfrost.

---
+++ TruckEx/TestGpioCore/Program.cs
@@ -0,0 +1,26 @@
+﻿using Bifrost.Devices.Gpio;
+using Bifrost.Devices.Gpio.Abstractions;
+using Bifrost.Devices.Gpio.Core;
+using System;
+
+namespace TestGpioCore
+{
+    class Program
+    {
+        private static IGpioController gpioController;
+
+        static void Main(string[] args)
+        {
+            gpioController = GpioController.Instance;
+
+            var pin = gpioController.OpenPin(2);
+
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            
+            Console.WriteLine("Going on");
+            pin.Write(GpioPinValue.High);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
